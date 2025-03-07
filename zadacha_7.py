bcc82722b9e4f8f4021ebf2dc76cf864d8f59aea
0wns666 <doncovanev58@gmail.com>
2025-03-07 23:11:36 +0200

    Add files via upload

---
+++ zadacha_7.py
@@ -0,0 +1,6 @@
+name_architect = input()
+number_of_projects_to_complete = int(input())
+total_time_needed_to_complete_all_projects = number_of_projects_to_complete * 3
+
+print(f"The architect {name_architect} will needed {total_time_needed_to_complete_all_projects} hours "
+      f"to complete all projects {number_of_projects_to_complete} projects/s.")
